a2f80ad33ccd1d21757f56422d6ee4129c7a8ade
tim01324 <tim01324@gmail.com>
2025-11-10 13:42:04 -0500

    Add @vercel/analytics for enhanced performance tracking in the application. Updated package.json and package-lock.json to include the new dependency, and integrated Analytics component in App.jsx for real-time analytics.

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,27 @@
-import React from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Layout from './components/Layout'
-import Home from './pages/Home'
-import Gallery from './pages/Gallery'
-import CV from './pages/CV'
-import Contact from './pages/Contact'
-import './App.css'
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout";
+import Home from "./pages/Home";
+import Gallery from "./pages/Gallery";
+import CV from "./pages/CV";
+import Contact from "./pages/Contact";
+import "./App.css";
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
-  return (
-    <Router>
-      <Layout>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/gallery" element={<Gallery />} />
-          <Route path="/cv" element={<CV />} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
-      </Layout>
-    </Router>
-  )
+	return (
+		<Router>
+			<Layout>
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route path="/gallery" element={<Gallery />} />
+					<Route path="/cv" element={<CV />} />
+					<Route path="/contact" element={<Contact />} />
+				</Routes>
+			</Layout>
+			<Analytics />
+		</Router>
+	);
 }
 
-export default App
-
+export default App;
